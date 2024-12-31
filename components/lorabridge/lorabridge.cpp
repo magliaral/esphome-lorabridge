09c0bbacc1ffed477b541e5f6e739d99e9e7511b
magliaral <49778555+magliaral@users.noreply.github.com>
2024-12-31 16:26:46 +0100

    Update lorabridge.cpp

--- components/lorabridge/lorabridge.cpp
+++ components/lorabridge/lorabridge.cpp
@@ -16,10 +16,17 @@ void LoRaBridge::loop() {
     static unsigned long last_log_time = 0; // Speichert die Zeit des letzten Log-Eintrags
     unsigned long current_time = millis();   // Aktuelle Zeit in Millisekunden seit dem Start
 
-    // Prüfen, ob seit dem letzten Log-Eintrag mehr als 5000 ms (5 Sekunden) vergangen sind
-    if (current_time - last_log_time > 15000) { // 15000 ms = 5 Sekunden
+    // Prüfen, ob seit dem letzten Log-Eintrag mehr als 15000 ms (15 Sekunden) vergangen sind
+    if (current_time - last_log_time > 15000) { // 15000 ms = 15 Sekunden
         ESP_LOGI(TAG, "Loop der LoRaBridge online läuft seit %lu ms", current_time);
         last_log_time = current_time; // Aktualisieren der letzten Log-Zeit
+    }
+
+    // Beispiel für zusätzliche Logik
+    if (state > 0) {
+        // Ihre Logik hier
+    } else {
+        // Ihre Logik hier
     }
 }
 
@@ -27,6 +34,5 @@ void LoRaBridge::dump_config(){
     ESP_LOGCONFIG(TAG, "LoRaBridge");
 }
 
-
-}  // namespace empty_component
+}  // namespace lorabridge
 }  // namespace esphome
